c9827e5064c6f8e1f866915f5cb89a8f429cf089
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-01-19 23:11:35 +0300

    Add rating to details page

--- src/conrollers/movieController.js
+++ src/conrollers/movieController.js
@@ -16,8 +16,10 @@ router.post('/create', (req, res) => {
 
 router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
-    const movie = movieService.getOne(movieId)
-    
+    const movie = movieService.getOne(movieId);
+    movie.rating = new Array(Number(movie.rating)).fill(true);
+    console.log(movie.rating);
+
     res.render('details', { movie })
 })
 
