9361dd78e6ea51e5fafc82382ff36bd68d8451db
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-01-19 18:01:36 +0300

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ const movieController = require('./conrollers/movieController.js');
 router.use(homeController);
 router.use(movieController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
 module.exports = router;
