f119d8b94000b92b6ccb8b8acf53a071a61b92a6
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-01-26 00:04:30 +0300

    Use mongoose for movie create

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,3 +1,5 @@
+const Movie = require('../models/Movie.js');
+
 const movies = [
     {
        _id: 1,
@@ -39,7 +41,8 @@ exports.getOne = (movieId) => {
     return movie;
 }
 
-exports.create = (movieData) => {
-    movieData._id = movies[movies.length - 1]._id + 1;
-    movies.push(movieData);
+exports.create = async (movieData) => {
+    const result = await Movie.create(movieData);
+
+    return result;
 }
