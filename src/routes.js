61e6b917f1075598140d95018cd82702f4801544
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-01-26 09:58:06 +0300

    Add create cast page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./conrollers/homeController.js');
 const movieController = require('./conrollers/movieController.js');
+const castController = require('./conrollers/castController.js');
 
 router.use(homeController);
 router.use(movieController);
+router.use('/cast' ,castController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
