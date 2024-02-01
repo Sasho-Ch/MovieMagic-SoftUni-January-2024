f4f6fa3ccbd27a48e08fcdb7a87ecf669ca48d3a
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-02-01 17:31:42 +0300

    Add register page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeController = require('./conrollers/homeController.js');
 const movieController = require('./conrollers/movieController.js');
 const castController = require('./conrollers/castController.js');
+const authController = require('./conrollers/authController.js');
 
 router.use(homeController);
 router.use(movieController);
 router.use('/cast' ,castController);
+router.use('/auth' ,authController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
