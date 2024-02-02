be396b812403fba7544b310698cd0b78f4064cb8
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-02-02 12:53:32 +0300

    Generate token on successful login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,37 @@
 const User = require('../models/User.js');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt.js');
 
+const SECRET = 'fsaeofn23fr89234hj9f8034hj09gf82h30df89hsdok';
+
+//TODO: Check if user exists
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+
+    //Get user form db
+    const user = await User.findOne({email});
+
+    // Check of user exists
+    if(!user) {
+        throw new Error('Cannot find email or password!');
+    }
+
+    //Check if password is valid
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid) {
+        throw new Error(`Cannot find email or password!`);
+
+    }
+
+    //Generate jwt token
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    }
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2h' });
+    //return token
+
+    return token;
+}
