a6825834af76e2ccda8fc6d666756c72e40bfe67
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-02-02 14:40:33 +0300

    Add route gard

--- src/conrollers/movieController.js
+++ src/conrollers/movieController.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 
 const movieService = require('../services/movieService.js');
 const castService = require('../services/castService.js');
+const { isAuth } = require('../middlewares/authMiddleware.js')
 
-router.get('/create', (req, res) => {
+router.use(isAuth);
+
+router.get('/create', isAuth ,(req, res) => {
     res.render('create')
 })
 
-router.post('/create',async (req, res) => {
+router.post('/create', isAuth ,async (req, res) => {
     const newMovie = req.body;
 
     try {
@@ -31,14 +34,14 @@ router.get('/movies/:movieId', async (req, res) => {
     res.render('details', { movie }) 
 });
 
-router.get('/movies/:movieId/attach', async (req, res) => {
+router.get('/movies/:movieId/attach', isAuth ,async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean(); 
     const casts = await castService.getAll().lean();
     //TODO: Remove already added casts
     res.render('movie/attach', { ...movie, casts })
 });
 
-router.post('/movies/:movieId/attach', async (req, res) => {
+router.post('/movies/:movieId/attach', isAuth ,async (req, res) => {
     const castId = req.body.cast;
     const movieId = req.params.movieId;
   
@@ -47,8 +50,7 @@ router.post('/movies/:movieId/attach', async (req, res) => {
     res.redirect(`/movies/${movieId}/attach`);
 });
 
-router.get('/movies/:movieId/edit', async (req, res) => {
-    
+router.get('/movies/:movieId/edit', isAuth ,async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     
     res.render('movie/edit', { movie })
---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('../lib/jwt.js');
+const { SECRET } = require('../config/config.js');
+
+exports.auth = async (req, res, next) => {
+    // get token
+    const token = req.cookies['auth'];
+
+    if (!token) { 
+        return next();
+
+    }
+
+    try {
+        const decodedToken = await jwt.verify(token, SECRET)
+        
+        req.user = decodedToken;
+
+        next();
+    } catch {
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
+    }
+
+}
+
+exports.isAuth = (req, res, next) => {
+    if(!req.user) {
+        res.redirect('/auth/login')
+    }
+
+    next();
+}
